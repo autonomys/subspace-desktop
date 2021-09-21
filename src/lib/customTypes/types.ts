5747d7f9703f30d45316e4ad74f491abecdc38e0
1devndogs <leo@subspace.network>
2021-09-21 03:59:10 -0300

    Add ts definitions for FarmerId, Solution, Slot, PocPreDigest

---
+++ src/lib/customTypes/types.ts
@@ -0,0 +1,19 @@
+import { AccountId } from '@polkadot/types/interfaces/runtime';
+import type { Bytes, Struct, U8aFixed, u64 } from '@polkadot/types';
+
+export interface FarmerId extends AccountId { }
+
+export interface Solution extends Struct {
+  readonly public_key: FarmerId;
+  readonly nonce: u64;
+  readonly encoding: Bytes;
+  readonly signature: Bytes;
+  readonly tag: U8aFixed;
+}
+export interface Slot extends u64 { }
+
+export interface PoCPreDigest extends Struct {
+  readonly slot: Slot;
+  readonly solution: Solution;
+}
+
