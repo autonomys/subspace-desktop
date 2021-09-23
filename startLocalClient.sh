269a240045ab5d17b365a724f5394f478ad46fd0
jdheeter <johndheeter@gmail.com>
2021-09-22 23:48:50 -0500

    revert change to startLocalClient

--- startLocalClient.sh
+++ startLocalClient.sh
@@ -4,6 +4,7 @@ docker pull subspacelabs/node-template-spartan
 docker run --rm --init -it \
 --net spartan \
 --name node-template-spartan-full \
+--mount source=spartan-node-template,target=/var/spartan \
 --publish 0.0.0.0:30333:30333 \
   --publish 127.0.0.1:9944:9944 \
   --publish 127.0.0.1:9933:9933 \
