defb800edb13c4688b52f1ebb03618a27a18abc6
Ozgun Ozerk <ozgunozerk@sabanciuniv.edu>
2021-12-15 14:33:41 +0300

    farming demo works

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,17 +7,32 @@
 mod windows;
 
 use serde::Serialize;
+use std::fs;
 use std::path::PathBuf;
 use tauri::{
   api::{self},
   CustomMenuItem, Event, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
 };
-use subspace_farmer::{Commitments, Farming, Identity, ObjectMappings, Plot, Plotting, WsRpc};
+use anyhow::{anyhow, Result};
+use log::info;
+use jsonrpsee::ws_server::WsServerBuilder;
+use std::net::SocketAddr;
+use subspace_farmer::ws_rpc_server::{RpcServer,RpcServerImpl};
+use subspace_farmer::{
+  Commitments, Farming, Identity, ObjectMappings, Plot, Plotting, RpcClient, WsRpc,
+};
+use subspace_solving::SubspaceCodec;
 
 #[derive(Serialize)]
 struct DiskStats {
   free_bytes: u64,
   total_bytes: u64,
+}
+
+#[tauri::command]
+async fn farming(){
+  let path = get_path(None);
+  let _ = farm(path, "ws://127.0.0.1:9944", "127.0.0.1:9955".parse().unwrap()).await;
 }
 
 #[tauri::command]
@@ -37,7 +52,8 @@ fn get_this_binary() -> PathBuf {
   return bin.unwrap();
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
   let quit = CustomMenuItem::new("quit".to_string(), "Quit");
   let toggle_visibility = CustomMenuItem::new("toggle_visibility".to_string(), "Hide");
 
@@ -78,18 +94,20 @@ fn main() {
     })
     .invoke_handler(
       #[cfg(not(target_os = "windows"))]
-      tauri::generate_handler![get_disk_stats, get_this_binary],
+      tauri::generate_handler![get_disk_stats, get_this_binary, farming],
       #[cfg(target_os = "windows")]
       tauri::generate_handler![
         windows::winreg_get,
         windows::winreg_set,
         windows::winreg_delete,
         get_this_binary,
-        get_disk_stats
+        get_disk_stats,
+        farming,
       ],
     )
     .build(tauri::generate_context!())
     .expect("error while running tauri application");
+
   app.run(|app_handle, e| match e {
     Event::CloseRequested { label, api, .. } => {
       let app_handle = app_handle.clone();
@@ -109,5 +127,104 @@ fn main() {
       api.prevent_exit();
     }
     _ => {}
+  });
+
+
+  Ok(())
+}
+
+/// Start farming by using plot in specified path and connecting to WebSocket server at specified
+/// address.
+pub(crate) async fn farm(
+  base_directory: PathBuf,
+  node_rpc_url: &str,
+  ws_server_listen_addr: SocketAddr,
+) -> Result<(), anyhow::Error> {
+  // TODO: This doesn't account for the fact that node can
+  // have a completely different history to what farmer expects
+  info!("Opening plot");
+  let plot = Plot::open_or_create(&base_directory).await?;
+
+  info!("Opening commitments");
+  let commitments = Commitments::new(base_directory.join("commitments").into()).await?;
+
+  info!("Opening object mapping");
+  let object_mappings = tokio::task::spawn_blocking({
+    let base_directory = base_directory.clone();
+
+    move || ObjectMappings::open_or_create(&base_directory)
   })
+    .await??;
+
+  info!("Connecting to node at {}", node_rpc_url);
+  let client = WsRpc::new(node_rpc_url).await?;
+
+  let farmer_metadata = client
+    .farmer_metadata()
+    .await
+    .map_err(|error| anyhow::Error::msg(error.to_string()))?;
+
+  let identity = Identity::open_or_create(&base_directory)?;
+
+  let subspace_codec = SubspaceCodec::new(&identity.public_key());
+
+  // Start RPC server
+  let ws_server = WsServerBuilder::default()
+    .build(ws_server_listen_addr)
+    .await?;
+  let ws_server_addr = ws_server.local_addr()?;
+  let rpc_server = RpcServerImpl::new(
+    farmer_metadata.record_size,
+    farmer_metadata.recorded_history_segment_size,
+    plot.clone(),
+    object_mappings.clone(),
+    subspace_codec,
+  );
+  let _stop_handle = ws_server.start(rpc_server.into_rpc())?;
+
+  info!("WS RPC server listening on {}", ws_server_addr);
+
+  // start the farming task
+  let farming_instance =
+    Farming::start(plot.clone(), commitments.clone(), client.clone(), identity);
+
+  // start the background plotting
+  let plotting_instance = Plotting::start(
+    plot,
+    commitments,
+    object_mappings,
+    client,
+    farmer_metadata,
+    subspace_codec,
+  );
+
+  tokio::select! {
+        res = plotting_instance.wait() => if let Err(error) = res {
+            return Err(anyhow!(error))
+        },
+        res = farming_instance.wait() => if let Err(error) = res {
+            return Err(anyhow!(error))
+        },
+    }
+
+  Ok(())
+}
+
+fn get_path(custom_path: Option<PathBuf>) -> PathBuf {
+  // set storage path
+  let path = custom_path
+    .or_else(|| std::env::var("SUBSPACE_DIR").map(PathBuf::from).ok())
+    .unwrap_or_else(|| {
+      dirs::data_local_dir()
+        .expect("Can't find local data directory, needs to be specified explicitly")
+        .join("subspace")
+    });
+
+  if !path.exists() {
+    fs::create_dir_all(&path).unwrap_or_else(|error| {
+      panic!("Failed to create data directory {:?}: {:?}", path, error)
+    });
+  }
+
+  path
 }
