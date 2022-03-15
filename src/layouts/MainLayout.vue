b2a9a270a9d2cdd40459b08d73fca79ebcb4191c
1devNdogs <vsalgado.a@gmail.com>
2022-03-15 10:04:39 -0300

    Frontend, fix info links, add testnet label (#94)

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,27 +4,35 @@ q-layout(view="hHh lpr fFf")
     q-toolbar
       q-img(no-spinner src="subspacelogo.png" width="150px")
       q-toolbar-title
-        // Show the dashboard status indicator when on the dashboard page.
-        .row(v-if="$route.name == 'dashboard'")
+        .row
           .col-auto.q-mr-md.relative-position
+            q-badge(color="grey" text-color="white")
+              .q-pa-xs(style="font-size: 14px") {{ lang.nonIncentivizedLabel }}
+            q-tooltip
+              .col
+                p.no-margin(style="font-size: 12px") {{ lang.nonIncentivizedTooltip }}
+          // Show the dashboard status indicator when on the dashboard page. 
+          .col-auto.q-mr-md.relative-position(
+            v-if="$route.name == 'dashboard'"
+          )
             q-icon(
               color="green-5"
               name="trip_origin"
-              size="40px"
+              size="28px"
               style="bottom: 0px; right: 5px"
               v-if="global.status.state == 'live'"
             )
             q-icon(
               color="yellow-8"
               name="trip_origin"
-              size="40px"
+              size="28px"
               style="bottom: 0px; right: 5px"
               v-if="global.status.state == 'loading'"
             )
             q-tooltip
               .col
                 p Farmer Status:
-                h6.no-margin {{ global.status.message }}
+                p <b>{{ global.status.message }}</b>
       div
         q-btn(flat icon="settings" round)
           MainMenu
@@ -38,6 +46,7 @@ import { defineComponent } from "vue"
 import { globalState as global } from "src/lib/global"
 import * as util from "src/lib/util"
 import MainMenu from "components/mainMenu.vue"
+const lang = global.data.loc.text.mainMenu
 
 export default defineComponent({
   name: "MainLayout",
@@ -46,6 +55,7 @@ export default defineComponent({
 
   data() {
     return {
+      lang,
       global: global.data,
       util,
       autoLaunch: false
