915e36b7768eaa64ab84b0054d8dcb3d78c0294c
Özgün Özerk <ozgun@subspace.network>
2022-03-29 14:13:57 +0300

    Typos (#123)
    telegram link fix, typos, and version bump

--- src/components/introModal.vue
+++ src/components/introModal.vue
@@ -27,13 +27,13 @@ mixin page3
   .row.q-mb-md
     ul
       li
-        p Did you know subspace is the first Proof of Capacity blockchain on Substrate?
+        p Did you know Subspace is the first Proof of Capacity blockchain on Substrate?
       li
-        p Did you know that subspace stores all data On-Chain!
+        p Did you know that Subspace stores all data On-Chain?
       li
         p You can talk with us on #[a(href="https://discord.gg/5MAp8CD684" target="_blank") Discord]
       li
-        p You can talk with us on #[a(href="https://t.me/subspacelabs" target="_blank") Telegram]
+        p You can talk with us on #[a(href="https://t.me/subspace_network" target="_blank") Telegram]
       li
         p Visit us on #[a(href="https://twitter.com/NetworkSubspace" target="_blank") Twitter]
       li
