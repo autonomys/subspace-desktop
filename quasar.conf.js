c3ea7bcbb01e6f98f62ddd059267cfeb02af6719
Ozgun Ozerk <ozgun@subspace.network>
2022-04-29 20:54:30 +0300

    suggestions

--- quasar.conf.js
+++ quasar.conf.js
@@ -45,7 +45,6 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      // env: require('dotenv').config().parsed,
       env: {
         PUBLIC_API_WS: "wss://aries-farm-rpc-b.subspace.network",
         LOCAL_API_WS: "ws://localhost:9944",
