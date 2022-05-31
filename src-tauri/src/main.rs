02e9d68f61154497429d5eae939b9d925df1d7ff
Serge Kovbasiuk <isserge@protonmail.com>
2022-05-31 15:42:55 +0300

    Start Devtools programmatically in dev

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -112,6 +112,15 @@ async fn main() -> Result<()> {
     let ctx = tauri::generate_context!();
     let id = &ctx.config().tauri.bundle.identifier;
     let app = tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+            let window = app.get_window("main").unwrap();
+            window.open_devtools();
+            window.close_devtools();
+            }
+            Ok(())
+        })
         .plugin(
             LoggerBuilder::new()
                 .targets(vec![
