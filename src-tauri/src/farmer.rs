b9b828e74ff9b66e7ad66a3b2a61dd5bd68a7c2c
Özgün Özerk <ozgun@subspace.network>
2022-07-27 11:16:03 +0300

    empty vec for listen_on

--- src-tauri/src/farmer.rs
+++ src-tauri/src/farmer.rs
@@ -131,7 +131,7 @@ async fn farm(base_directory: PathBuf, farm_args: FarmingArgs) -> Result<(), any
 
     // Starting the relay server node.
     let (relay_server_node, mut relay_node_runner) = subspace_networking::create(Config {
-        listen_on: listen_on.clone(),
+        listen_on,
         allow_non_globals_in_dht: true,
         ..Config::with_generated_keypair()
     })
@@ -169,7 +169,7 @@ async fn farm(base_directory: PathBuf, farm_args: FarmingArgs) -> Result<(), any
             enable_dsn_archiving: matches!(archiving, ArchivingFrom::Dsn),
             enable_dsn_sync: dsn_sync,
             enable_farming: true,
-            listen_on,
+            listen_on: vec![],
             relay_server_node: Some(relay_server_node),
         },
         usable_space,
