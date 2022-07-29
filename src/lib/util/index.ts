9f0a67262684ed5f9ab9eb533d912e520789df4c
Serge Kovbasiuk <isserge@protonmail.com>
2022-07-29 10:14:38 +0300

    move tauri utils to a separate modules (otherwise tests fail to run)

---
+++ src/lib/util/index.ts
@@ -0,0 +1,23 @@
+export {
+  IUtil,
+  random,
+  showModal,
+  toFixed,
+  plotTimeMsEstimate,
+  formatMS,
+  promiseTimeout,
+  PIECE_SIZE,
+  GB,
+  CONTEXT_MENU,
+  createApi,
+  generateNodeName,
+  getErrorMessage,
+} from './util';
+
+export {
+  appName,
+  getLogPath,
+  errorLogger,
+  infoLogger,
+  resetAndClear,
+} from './tauri';
